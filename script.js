1c31acdde386a88eb10cf583866aa1f1f767c802
Davieb1 <121017739+Davieb1@users.noreply.github.com>
2024-08-17 09:39:29 +0100

    Initial File Upload
    Sport & Fitness Education website

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showModal(elementId) {
+    document.getElementById(elementId).style.display = "block";
+}
+
+function closeModal(elementId) {
+    document.getElementById(elementId).style.display = "none";
+}
